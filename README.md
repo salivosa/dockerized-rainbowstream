4ab4ad2a59d1cded830e631a0ee4ced1faeaf60b
sali <salvadore0ran@gmail.com>
2023-01-22 11:27:44 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-send tweets through commands!
+send tweets through commands using a dockerized Rainbow Stream!
 
-kinda exploiting rainbow stream App usage using screen shenanigans and docker containers.
+no need to register a new Twitter app, you can avoid a permaban like me with this [other project][1]
+
+you can use container non-interactive using screen.
+
+[1]: https://github.com/salivosa/TwitterOps "other project"
--- set_rainbowstream.sh
+++ set_rainbowstream.sh
@@ -1,59 +1,21 @@
 #!/bin/bash
 
-alias twitter_cli='/home/salivosa/scripts/docker/sali-rainbowstream/send_tweet_dockerized_rainbowstream.sh'
-
-docker stop rainbowstream_salivosx;
-docker rm rainbowstream_salivosx;
-
-docker stop rainbowstream_saribosu;
-docker rm rainbowstream_saribosu;
-
-docker stop rainbowstream_bluetsutsu;
-docker rm rainbowstream_bluetsutsu;
-
-docker build -t rainbowstream "/home/salivosa/scripts/docker/sali-rainbowstream"
+docker build -t rainbowstream .
 
 # ------ configure keys ------
 
-#salivosx
-#docker run -ti --rm -v "/media/SL HDD/DOCKER/twitter_cli/salivosx":/root/ rainbowstream rainbowstream
-
-#saribosu
-#docker run -ti --rm -v "/media/SL HDD/DOCKER/twitter_cli/saribosu":/root/ rainbowstream rainbowstream
-
-#bluetsutsu
-#docker run -ti --rm -v "/media/SL HDD/DOCKER/twitter_cli/bluetsutsu":/root/ rainbowstream rainbowstream
+#twitter_user
+#docker run -ti --rm -v "/home/user/twitter_user":/root/ rainbowstream rainbowstream
 
 
-# ------ deploy containers ------
-
-#salivosx
+# ------ deploy container/s ------
 
 docker run -d \
-  --name rainbowstream_salivosx \
+  --name rainbowstream_twitter_user \
   --privileged \
   --restart=unless-stopped \
-  -v "/media/SL HDD/DOCKER/twitter_cli/salivosx/.rainbow_oauth":/root/.rainbow_oauth \
+  -v "/home/user/twitter_user":/root/.rainbow_oauth \
   rainbowstream
 
-#saribosu
-
-docker run -d \
-  --name rainbowstream_saribosu \
-  --privileged \
-  --restart=unless-stopped \
-  -v "/media/SL HDD/DOCKER/twitter_cli/saribosu/.rainbow_oauth":/root/.rainbow_oauth \
-  rainbowstream
-
-#bluetsutsu
-
-docker run -d \
-  --name rainbowstream_bluetsutsu \
-  --privileged \
-  --restart=unless-stopped \
-  -v "/media/SL HDD/DOCKER/twitter_cli/bluetsutsu/.rainbow_oauth":/root/.rainbow_oauth \
-  rainbowstream
-
-
-#example
-#docker exec -it rainbowstream_salivosx screen -S screen_rainbow -p 0 -X stuff "t $tweet ^M"
+#usage example
+#docker exec -it rainbowstream_twitter_user screen -S screen_rainbow -p 0 -X stuff "t いいですね ^M"
